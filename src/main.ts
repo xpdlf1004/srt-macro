c943500ec0d1599dfbe6ce389424eec243fa03a0
Gilyoung <roadzero@kodebox.io>
2019-01-19 22:35:46 +0900

    Import the server code and the bootstrap lib

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import BootstrapVue from "bootstrap-vue";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
     router,
