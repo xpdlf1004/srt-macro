91ade3ae325ed0c848653fd0a1946470a8912bf7
Park Juhyung <majecty@gmail.com>
2019-01-30 15:40:36 +0000

    Add build script

---
+++ build-prepare.sh
@@ -0,0 +1,16 @@
+
+# set -e : exit when one of the below commands are failed
+set -e
+
+yarn run build
+yarn tsc -p ./tsconfig.server.json
+
+mkdir -p build/electron
+cp electron/main.ts build/electron/
+yarn tsc build/electron/main.ts
+
+cp ./build.package.json ./build/package.json
+
+(cd build && ../node_modules/.bin/ncc build electron/main.js -o ./)
+
+cp -r dist build/
